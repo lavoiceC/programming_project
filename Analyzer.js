1a439ca4375f3f97aa52201d0d6fb725a0211563
lavoiceC <lavoicel@student.elms.edu>
2026-03-30 10:54:09 -0400

    Delete Analyzer.js

--- Analyzer.js
+++
@@ -1,6 +0,0 @@
-let job_title = {};
-job_title["JD001"] = "Software Engineer";
-job_title["JD002 "] = "Customer Support Associate";
-job_title["JD003"] = "Data Analyst";
-job_title["JD004 "] = "Market Coordinator";
-job_title["JD005 "] = "Adminstrative Assistant";
