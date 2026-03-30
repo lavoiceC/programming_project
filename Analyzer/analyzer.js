35d046d4faeeb982c772e23602b0ab9aadada181
lavoiceC <lavoicel@student.elms.edu>
2026-03-30 01:54:33 -0400

    Add files via upload

---
+++ Analyzer/analyzer.js
@@ -0,0 +1,78 @@
+let rules = null;
+
+async function loadRubrics() {
+    const response = await fetch("rubric.json");
+    const data = await response.json();
+    rules = data[0]
+    console.log("Rubric loaded:", rules)
+}
+
+loadRubrics();
+
+document.getElementById("jdForm").addEventListener("submit", function(event) {   
+     event.preventDefault();
+
+    const jd = document.getElementById("JobDescription").value.trim();
+    const results = document.getElementById("results");
+    var jdLower = jd.toLowerCase();
+
+    if (jd.length < 50){
+        alert("Please enter 50 characters")
+        return;
+    }
+    //readability detection 
+
+ const words = jd.length;
+  var readability;
+  if (words < 80) {
+    readability = "Very easy";
+  }
+
+else if (words < 150) {
+    readability = "Easy";
+  }
+
+   if (words < 250) {
+    readability = "Moderate";
+  }
+  else{
+    readability = "Hard";
+  }
+    //document.getElementById("bias").textContent = "Gender based language"
+//bia detection
+    var biasFound = rules.bias_terms.filter(function(term) {
+        return jdLower.indexOf(term.toLowerCase()) > -1;
+    });
+
+    var biasOutput;
+        if (biasFound.length > 0) {
+            biasOutput = biasFound.map(function(term) {
+                var type = rules.bias_types[term] || "General Bias";
+                return term + " (" + type + ")";
+            }).join(", ");
+        } else {
+            biasOutput = "bias not detected";
+        }
+
+        // Accessibility 
+    var accessFound = rules.accessibility_terms.filter(function(term) {
+        return jdLower.indexOf(term.toLowerCase()) > -1;
+    });
+
+    var accessOutput;
+        if (accessFound.length > 0) {
+            accessOutput = "Accessibility terms found: " + accessFound.join(", ");     
+           } else {
+            accessOutput = "no accessibility found";
+        }
+    
+
+
+    document.getElementById("OverallScore").textContent = 78;
+    document.getElementById("Readability").textContent = readability
+    document.getElementById("bias").textContent = biasOutput;
+    document.getElementById("accessibility").textContent = accessOutput;
+    document.getElementById("recommendations").textContent = "add accomodations"
+    document.getElementById("Compliance")
+    results.style.display = "block";
+});
